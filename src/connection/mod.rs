c846c28c8e2c544d9e0ead6687b03305d1efea13
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-09-16 10:35:07 -0700

    `src/connection/mod.rs`: added a US-ASCII check, added building blocks for multiline command parsing

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -113,6 +113,16 @@ async fn handle_smtp_command(
         return Ok(ShouldClose::Keep); // Should this close the session?
     }
 
+    // RFC 5321 uses US-ASCII, specifically ANSI X3.4-1968 (reference 6).
+    // As far as I can tell, [`std::ascii:Char`] upholds a standard that is functionally equivalent
+    // for the purposes of this library.
+    //
+    // https://www.rfc-editor.org/rfc/rfc5321.html#ref-6
+    if !line.is_ascii() {
+        write_line!(write_stream, "500 Syntax error - invalid character")?;
+        return Ok(ShouldClose::Keep); // Should this close the session?
+    }
+
     // Trim whitespace from line.
     //
     // RFC 5321 section 4.1.1 recommends to allow for trailing whitespace.
@@ -126,9 +136,19 @@ async fn handle_smtp_command(
     // the only case-sensitive part of an SMTP command, so `text` is not set to uppercase.
     //
     // https://www.rfc-editor.org/rfc/rfc5321.html#section-2.4
-    let (command, text) = match trimmed.split_once(' ') {
-        Some((c, t)) => (c.to_uppercase(), Some(t)),
-        None => (trimmed.to_uppercase(), None),
+    let (command, text) = match trimmed.split_once([' ', '-']) {
+        Some((c, t)) => (c.to_ascii_uppercase(), Some(t)),
+        None => (trimmed.to_ascii_uppercase(), None),
+    };
+
+    let is_multiline = match trimmed
+        .chars()
+        .nth(command.chars().count())
+        .expect("a string must contain a substring of itself")
+    {
+        '-' => MultiLine::HasNext,
+        ' ' => MultiLine::LastLine,
+        _ => unreachable!("`command` will only split on `' '` or `'-'`"),
     };
 
     if command == "QUIT" {
@@ -160,4 +180,24 @@ enum CloseReason {
     TimedOut(Elapsed),
     /// The TCP connection was forcefully ended by the client.
     ClosedByClient,
+}
+
+/// Indicates if the parsed command is the last line to be parsed before replying.
+#[derive(PartialEq, Eq, Debug)]
+enum MultiLine {
+    /// This is the last line to be parsed before replying.
+    LastLine,
+    /// This is not the last line to be parsed before replying, there will be more incoming.
+    HasNext,
+}
+
+impl MultiLine {
+    /// Get the character used to split the verb and text of an SMTP command.
+    #[must_use]
+    pub const fn split(&self) -> char {
+        match self {
+            Self::LastLine => ' ',
+            Self::HasNext => '-',
+        }
+    }
 }
